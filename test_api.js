1b909a954410c66403af577c80a9065f44780d50
Aasav Ravi <aasavravi@gmail.com>
2025-10-31 01:41:18 +0530

    Remove required from username in User model

--- test_api.js
+++ test_api.js
@@ -1,9 +1,9 @@
 const https = require('https');
 
 const postData = JSON.stringify({
-  name: 'Test User23',
-  email: 'test23@example.com',
-  phone: '1234567803',
+  name: 'Test User24',
+  email: 'test24@example.com',
+  phone: '1234567804',
   password: 'password123',
   role: 'candidate',
   dob: '1990-01-01',
