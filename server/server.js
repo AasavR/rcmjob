35944da1a24ea6b8a23479055e01229e16bbf592
Aasav Ravi <aasavravi@gmail.com>
2025-11-03 18:52:38 +0530

    Fix CORS preflight issues for production
    - Added OPTIONS method to allowed methods
    - Added allowedHeaders for Content-Type and Authorization
    - This resolves the CORS preflight request blocking in production

--- server/server.js
+++ server/server.js
@@ -30,8 +30,9 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
